7b770f622a4894ff71a2b436025488dca1e36cb4
Serg <bkuek@mail.ru>
2017-02-26 17:14:56 +0200

    Initial commit

---
+++ ReogglAPI.py
@@ -0,0 +1,226 @@
+import requests
+import json
+from datetime import date, time, datetime
+import random
+from urllib.parse import urlparse
+
+
+class ReTogglAPI:
+    """Implements some general API calls for CyberHull ReToggl app"""
+    params = {}
+    common_headers = {}
+    date_format = "%Y-%m-%d"
+    time_format = "%H:%M:%S"
+    datetime_format = "%sT%s" % (date_format, time_format)
+    rt_projects = {}
+    rt_latest_tasks = {}
+
+    class SearchHelper:
+        """Provides easy to use search in ReTogglEntry lists"""
+
+        @staticmethod
+        def search_by(where, what, items):
+            results = {}
+            _ = [results.update({k: item}) if what in getattr(item, where) else None for k, item in items.items()]
+            return results
+
+    class ReTogglEntry:
+        """Common parent for any ReToggl entry"""
+
+        def as_json(self):
+            return self._as_json(self)
+
+        @staticmethod
+        def _as_json(obj):
+            def __try_to_str(o):
+                try:
+                    return o.__dict__
+                except:
+                    if type(o) is datetime:
+                        return o.strftime(ReTogglAPI.datetime_format)
+                    if type(o) is time:
+                        return o.strftime(ReTogglAPI.time_format)
+                    if type(o) is date:
+                        return o.strftime(ReTogglAPI.date_format)
+                    if type(o) is ReTogglAPI.ReTogglTimeEntry:
+                        return o.as_json()
+                    return str(o)
+
+            return json.dumps(
+                obj,
+                default=lambda o: __try_to_str(o),
+                sort_keys=True,
+                indent=0,
+                separators=(',', ':')
+            ).replace('\n', '')
+
+    class ReTogglTimeEntry(ReTogglEntry):
+        """Time entry holds information about every user's activity: time, task and so on"""
+        def __init__(self,
+                     end_date=datetime.today(),
+                     name='',
+                     project_id='',
+                     start_date=datetime.today(),
+                     time_zone_offset_minutes=0,
+                     user_id='',
+                     id=0,
+                     deleted=False):
+            self.end_date = end_date
+            self.name = name
+            self.project_id = project_id
+            self.start_date = start_date
+            self.time_zone_offset_minutes = time_zone_offset_minutes
+            self.user_id = user_id
+            self.id = id
+            self.deleted = deleted
+
+        def as_new_json(self):
+            """Get JSON data for the entity. Server needs special JSON format to create new entries"""
+            o = [{
+                'temporary_id': random.randint(0, 99999999),
+                'time_entry': {
+                    'user_id': self.user_id,
+                    'name': self.name,
+                    'project_id': self.project_id,
+                    'start_date': self.start_date,
+                    'end_date': self.end_date,
+                    'time_zone_offset_minutes': self.time_zone_offset_minutes,
+                    'deleted': self.deleted
+                }
+            }]
+            return self._as_json(o)
+
+        @staticmethod
+        def from_dict(data):
+            """Construct ReTogglTimeEntry using dict"""
+            return ReTogglAPI.ReTogglTimeEntry(
+                end_date=datetime.strptime(data['end_date'], ReTogglAPI.datetime_format),
+                name=data['name'],
+                project_id=data['project_id'],
+                start_date=datetime.strptime(data['start_date'], ReTogglAPI.datetime_format),
+                time_zone_offset_minutes=data['time_zone_offset_minutes'],
+                user_id=data['user_id'],
+                id=data['id']
+            )
+            pass
+
+    class ReTogglProject(ReTogglEntry):
+        """Project holds general information about a number of time entries"""
+        __default_project = {
+            'active': True,
+            'client_id': '',
+            'color': '#FF0000',
+            'id': random.randint(1, 9999999999),
+            'name': ''
+        }
+
+        def __init__(self, project=__default_project):
+            [setattr(self, attr, value) for attr, value in project.items()]
+
+    def __init__(self, params, verbose=lambda x, y: None):
+        """Create new ReTogglAPI entity. Use instance of the class to access ReToggl API.
+
+        Keyword arguments:
+            params  --  common configurations
+            verbose --  message output method
+        """
+        self.params = params
+        self.verbose = verbose
+        endpoint = urlparse(self.params.endpoint)
+        self.common_headers = {
+            'Host': endpoint.netloc,
+            'User-Agent': params.user_agent,
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'CH-User-Token': params.ch_user_token,
+            'Referer': 'http://%s/' % endpoint.netloc,
+            'Connection': 'keep-alive'
+        }
+        self.verbose(3, "Ready to load project list")
+
+    def __get_json(self, method, url, **kwargs):
+        self.verbose(4, "[%s] %s" % (method, url))
+        response = requests.request(method, url, **kwargs)
+        j = json.loads(response.text)
+        return j
+
+    def get_projects(self):
+        """Load all projects from server"""
+        u_get_projects = "%sprojects?user_token=%s" % (
+            self.params.endpoint,
+            self.params.user_token
+        )
+        data = self.__get_json('get', u_get_projects, headers=self.common_headers)
+        projects = {proj['id']: self.ReTogglProject(proj) for (proj) in data['data']}
+        self.verbose(2, "Loaded %i known projects" % (len(projects)))
+        return projects
+
+    def get_tasks(self, start_date, end_date):
+        """Get time entries from server in date range"""
+        s_start_date, s_end_date = start_date.strftime(self.date_format), end_date.strftime(self.date_format)
+        u_get_tasks = "%stime-entries?user_token=%s&end_date=%s&start_date=%s" % (
+            self.params.endpoint,
+            self.params.user_token,
+            s_end_date,
+            s_start_date
+        )
+        data = self.__get_json('get', u_get_tasks, headers=self.common_headers)
+        tasks = {ti['id']: self.ReTogglTimeEntry(ti) for (ti) in data['data']['time_entries']}
+        self.verbose(2, "Loaded %i time entries from %s to %s" % (len(tasks), s_start_date, s_end_date))
+        return tasks
+
+    def get_latest_tasks(self):
+        """Get time entries for last month"""
+        # FIXME: Don't ask my why it works with 8th days of month only
+        date_from = date(year=date.today().year, month=date.today().month - 1, day=8)
+        date_to = date(year=date_from.year, month=date.today().month + 1, day=8)
+        return self.get_tasks(date_from, date_to)
+
+    def new_time_entry(self, task: ReTogglTimeEntry):
+        """Push time entry to the server"""
+        json_task = task.as_new_json()
+        self.verbose(4, "Ready to push new task:\n%s" % (json_task))
+
+        u_new_task = "%stime-entries?user_token=%s" % (
+            self.params.endpoint,
+            self.params.user_token
+        )
+        result = self.__get_json('post', u_new_task, headers=self.common_headers, data=json_task)
+        return ReTogglAPI.ReTogglTimeEntry.from_dict(result['data'][0]['created_object'])
+
+    def delete_time_entry(self, te):
+        """Remove time entry from server"""
+        data = ReTogglAPI.ReTogglEntry()
+        data.id = te.id
+        data.deleted = True
+        data = '[' + data.as_json() + ']'
+        self.verbose(4, "Ready to delete task #%s" % (te.id))
+
+        u_del_task = "%stime-entries?user_token=%s" % (
+            self.params.endpoint,
+            self.params.user_token
+        )
+        result = self.__get_json('delete', u_del_task, headers=self.common_headers, data=data)
+        return ReTogglAPI.ReTogglTimeEntry.from_dict(result['data'][0]['updated_object'])
+
+# # Usage example
+#
+# # Create API client instance
+# tt = ReTogglAPI(args)
+# # Get existing project
+# proj = (ReTogglAPI.SearchHelper.search_by('name', 'Review Buzz: Dev', tt.rt_projects)[0])
+# # Create new time entry in the project
+# te = ReTogglAPI.ReTogglTimeEntry(
+#     end_date=datetime.today()+timedelta(minutes=15),
+#     name="TTT-123",
+#     project_id=proj.id,
+#     user_id=args.user_id
+# )
+# # Push it to server
+# te = tt.new_time_entry(te)
+# # Remove it from server
+# te = tt.delete_time_entry(te)
+#
+# # Please note that task list is also stored locally, in your browser,
+# # so you will see updates after re-login in the ReToggl. It is recommended
+# # to close ReToggl in your browser during API usage
---
+++ csv2rt.py
@@ -0,0 +1,185 @@
+import argparse
+from datetime import date, time, datetime, timedelta
+from ReogglAPI import ReTogglAPI
+import csv
+import sys
+import re
+
+parser = argparse.ArgumentParser(description='CSV --> ReToggl importer.')
+parser.add_argument('input', metavar='I', type=str, help='input CSV file')
+parser.add_argument('-u', '--user-agent', type=str,
+                    default='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0',
+                    help='user-agent header for HTTP requests')
+parser.add_argument('-e', '--endpoint', type=str,
+                    default='***REMOVED***',
+                    help='ReToggl API endpoint')
+parser.add_argument('-t', '--user-token', type=str,
+                    default='***REMOVED***',
+                    help='ReToggl API token')
+parser.add_argument('-i', '--user-id', type=str,
+                    default='***REMOVED***',
+                    help='ReToggl user id')
+parser.add_argument('--ch-user-token', type=str,
+                    default='***REMOVED***',
+                    help='CyberHull user token')
+parser.add_argument('-v', '--verbose', type=int,
+                    default=0,
+                    help='verbosity level. 0 is lowest, 4 - debug')
+parser.add_argument('-S', '--simulate', type=bool,
+                    default=True,
+                    help='simulation mode. Set to False to push tasks')
+
+args = parser.parse_args()
+
+
+def verbose(lvl, message):
+    """Logger function
+
+    Keyword arguments:
+        lvl     --  message priority level
+        message --  message text
+    """
+    hdr = {
+        -1: "SYS",
+        0: "STATUS",
+        1: "INFO",
+        2: "MSG",
+        3: "EXTRA",
+        4: "DBG"
+    }
+    if args.verbose >= lvl:
+        print("%s: %s" % (hdr[lvl], message))
+
+
+csv_date_format = "%d/%m/%Y"
+csv_time_format = "%H:%M"
+csv_date_time_format = "%s %s" % (csv_date_format, csv_time_format)
+
+rt_projects = []
+
+if args.simulate:
+    verbose(-1, "Simulation mode ON. Set -S to False to push tasks")
+
+
+class RBNameHelper:
+    class NameConversionError(Exception):
+        pass
+
+    NAME_TO_PROJECT_ID = 1
+    NAME_TO_TASK_NAME = 0
+
+    __prekrasnyy_format = r"^(?P<ticket>(\w*-\d*)|([a-zA-Z]*))(\s-\s*(?P<project>.*))*\s*-\s*(?P<general_project>RB)"
+
+    @staticmethod
+    def conv_task_name(name, conversion_method):
+        """Converts task name to project ID or to ReToggl task name
+
+        Keyword arguments:
+            name                --  the task name
+            conversion_method   --  set to NAME_TO_PROJECT_ID when conversion to project id is needed
+                                    or to NAME_TO_TASK_NAME when name prettifying needed
+        """
+        if conversion_method == RBNameHelper.NAME_TO_PROJECT_ID:
+            return RBNameHelper.__name_to_project_id(name)
+        if conversion_method == RBNameHelper.NAME_TO_TASK_NAME:
+            return RBNameHelper.__name_to_task_name(name)
+
+    @staticmethod
+    def __name_to_project_id(name):
+        items = RBNameHelper.__split(name)
+        # If ticket name matches pattern, use well-known project
+        if 'mnt-' in items["ticket"].lower():
+            return '000000276'  # Maintenance
+        if any(x in items["ticket"].lower() for x in ['cyb-', 'collaboration']):
+            return '000000084'  # Development
+        if 'et-' in items["ticket"].lower():
+            return '000000277'  # Elm Tree Project
+        if 'hive-' in items["ticket"].lower():
+            return '000000297'  # HIVE project
+        # Full search in ReviewBuzz projects
+        proj = ReTogglAPI.SearchHelper.search_by('name', 'Buzz', rt_projects)
+        proj = ReTogglAPI.SearchHelper.search_by('name', items["project"], proj)
+        if len(proj) <= 0:
+            raise RBNameHelper.NameConversionError("Cannot convert [%s] to project ID" % str(items["project"]))
+        return proj[0].id
+
+    @staticmethod
+    def __name_to_task_name(name):
+        items = RBNameHelper.__split(name)
+        # FIXME: dirty hack to apply ticket names
+        if 'collaboration' in items["ticket"].lower():
+            items["ticket"] = 'Calling, Collaboration'
+        return items["ticket"]
+
+    @staticmethod
+    def __split(name: str):
+        matched = re.match(RBNameHelper.__prekrasnyy_format, name).groupdict()
+        if len(matched) < 2:
+            raise RBNameHelper.NameConversionError("Cannot apply regexp to [%s]" % name)
+        return matched
+
+
+class ReTogglEntryList(ReTogglAPI.ReTogglEntry):
+    """Holds a list of ReTogglEntry"""
+    l = []
+
+    def append(self, item):
+        self.l.append(item)
+
+    def as_json(self):
+        return self._as_json(self.l)
+
+    def get_list(self):
+        return self.l
+
+
+pushed = ReTogglEntryList()
+
+try:
+    rt_api = ReTogglAPI(args, verbose)
+    rt_projects = rt_api.get_projects()
+    verbose(1, "Reading %s file" % args.input)
+    task_data = []
+    with open(args.input, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            task_data.append(row)
+    verbose(1, "%i tasks loaded" % len(task_data))
+    for task in task_data:
+        verbose(2, "Pushing task [%s] at [%s] to the server%s" % (task["Task"], task["Date/Time"], " (simulation)" if args.simulate else ""))
+        start_datetime = datetime.strptime(task["Date/Time"], csv_date_time_format)
+        try:
+            end_datetime = start_datetime + timedelta(hours=float(task["Decimal Hours"]))
+            time_entry = ReTogglAPI.ReTogglTimeEntry(
+                start_date=start_datetime,
+                end_date=end_datetime,
+                name=RBNameHelper.conv_task_name(task["Task"], RBNameHelper.NAME_TO_TASK_NAME).strip(),
+                project_id=RBNameHelper.conv_task_name(task["Task"], RBNameHelper.NAME_TO_PROJECT_ID),
+                user_id=args.user_id,
+                id=task["Id"]
+            )
+            if args.simulate:
+                pushed.append(time_entry)
+            else:
+                pushed.append(rt_api.new_time_entry(time_entry))
+                verbose(1, "Task [%s] at [%s] pushed" % (task["Task"], task["Date/Time"]))
+        except RBNameHelper.NameConversionError as err:
+            verbose(
+                -1,
+                "Can't convert name to project id and task name for task [%s] (id: %s) at [%s]: %s" %
+                (task["Task"], task["Id"], task["Date/Time"], str(err))
+            )
+            continue
+        except Exception as err:
+            verbose(
+                -1,
+                "Something wrong with server while pushing task [%s] at [%s]: %s. Check server state manually" %
+                (task["Task"], task["Date/Time"], str(err))
+            )
+            continue
+
+except Exception as err:
+    verbose(-1, "Something went wrong: {0}".format(err))
+    sys.exit(1)
+
+verbose(1, pushed.as_json())
