82e5eef636f702c1d9daeeded07dc256c96bbba5
Livich <sergiy.lilikovych@gmail.com>
2017-03-13 14:18:57 +0200

    RT API access test fix

--- test/test_retoggl_access.py
+++ test/test_retoggl_access.py
@@ -1,12 +1,18 @@
 from ReogglAPI import ReTogglAPI
+from argparse import Namespace
+import pytest
 
-def test_rt_access():
-    tt = ReTogglAPI({
-       "user_id": "***REMOVED***",
-       "endpoint": "***REMOVED***",
-       "user_agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0",
-       "ch_user_token": "***REMOVED***",
-       "user_token": "***REMOVED***"
-    })
-    rt_projects = tt.get_projects()
+@pytest.fixture()
+def api_setup():
+    rt_api =  ReTogglAPI(Namespace(
+       user_id = "***REMOVED***",
+       endpoint = "***REMOVED***",
+       user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0",
+       ch_user_token = "***REMOVED***",
+       user_token = "***REMOVED***"
+    ))
+    return rt_api
+
+def test_rt_access(api_setup):
+    rt_projects = api_setup.get_projects()
     assert len(rt_projects) > 0
