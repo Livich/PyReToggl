a11940137941f4c194eaa02f64465d481534eafe
Livich <sergiy.lilikovych@gmail.com>
2017-03-13 13:54:05 +0200

    RT API access test implementation

--- test/test_retoggl_access.py
+++ test/test_retoggl_access.py
@@ -1,4 +1,12 @@
 from ReogglAPI import ReTogglAPI
 
-def test_ci():
-    assert 1 == 1
+def test_rt_access():
+    tt = ReTogglAPI({
+       "user_id": "***REMOVED***",
+       "endpoint": "***REMOVED***",
+       "user_agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0",
+       "ch_user_token": "***REMOVED***",
+       "user_token": "***REMOVED***"
+    })
+    rt_projects = tt.get_projects()
+    assert len(rt_projects) > 0
