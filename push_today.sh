852aec157e3d3a500a13c08024c2496b1e6a4b6c
Livich <sergiy.lilikovych@gmail.com>
2017-03-15 13:24:12 +0200

    Implements push_today script

---
+++ push_today.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dt=$(date +'%d/%m/%Y')
+if [ -z "$1" ] || ([ "$1" != "0" ] && [ "$1" != "1" ]);
+then
+    echo "USAGE:  push_today [simulation]"
+    echo "        [simulation] to 1 to simulate time entry pushig process. Set to 0 otherwise."
+    exit
+fi
+python3 ./tw2rt.py -f ${dt}T00:01 -t ${dt}T23:59 -v 4 -S $1 ./config.ini
